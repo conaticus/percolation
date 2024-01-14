7873053dcfc42a1f60adf96f620545fa0d2263e7
Sam <enquiry.conaticus@gmail.com>
2024-01-14 20:22:14 +0000

    remove cursed for loops (rendering optimisations)

--- percolation.c
+++ percolation.c
@@ -9,10 +9,11 @@
 #define TOP_NODE_ID -1
 #define BOTTOM_NODE_ID -2
 
+#define BORDER_SIZE 1
+
 typedef struct {
     SDL_Rect rect;
     int root;
-    bool is_open;
 } GridCell;
 
 int get_hovered_cell_index(SDL_Rect* grid_cursor, GridDimensions* grid_dimensions) {
@@ -23,8 +24,8 @@ int get_hovered_cell_index(SDL_Rect* grid_cursor, GridDimensions* grid_dimension
 void run_simulation(GridDimensions* grid_dimensions, SDL_Renderer* renderer) {
     // Place the grid cursor in the middle of the grid.
     SDL_Rect grid_cursor = {
-        .x = (grid_dimensions->width - 1) / 2 * grid_dimensions->cell_size,
-        .y = (grid_dimensions->height - 1) / 2 * grid_dimensions->cell_size,
+        .x = (grid_dimensions->width - BORDER_SIZE) / 2 * grid_dimensions->cell_size,
+        .y = (grid_dimensions->height - BORDER_SIZE) / 2 * grid_dimensions->cell_size,
         .w = grid_dimensions->cell_size,
         .h = grid_dimensions->cell_size,
     };
@@ -40,23 +41,24 @@ void run_simulation(GridDimensions* grid_dimensions, SDL_Renderer* renderer) {
         int root = i;
 
         if (i + 1 > grid_dimensions->width) {
-            bool is_at_bottom_row = i + 1 > grid_dimensions->width * (grid_dimensions->height - 1);
+            bool is_at_bottom_row = i + 1 > grid_dimensions->width * grid_dimensions->height;
             root = is_at_bottom_row ? BOTTOM_NODE_ID : TOP_NODE_ID;
         }
 
         GridCell* grid_cell = &grid_cells[i];
         grid_cell->root = root;
-        grid_cell->is_open = false;
 
         // It's best not to ask
         int height_excess = i % grid_dimensions->height;
         grid_cell->rect = (SDL_Rect) {
            .x = height_excess * grid_dimensions->cell_size,
-           .y = ((i - height_excess) / grid_dimensions->height) * grid_dimensions->cell_size,
-           .w = grid_dimensions->cell_size,
-           .h = grid_dimensions->cell_size,
+           .y = (((i - height_excess) / grid_dimensions->height) * grid_dimensions->cell_size),
+           .w = grid_dimensions->cell_size - BORDER_SIZE,
+           .h = grid_dimensions->cell_size - BORDER_SIZE,
         };
     }
+
+    sdl_draw_grid_outlines(grid_dimensions, renderer);
 
     while (!quit) {
         SDL_Event event;
@@ -78,21 +80,13 @@ void run_simulation(GridDimensions* grid_dimensions, SDL_Renderer* renderer) {
     
         if (mouse_clicked) {
             int cell_index = get_hovered_cell_index(&grid_cursor, grid_dimensions);
-            grid_cells[cell_index].is_open = true;
+            GridCell* grid_cell = &grid_cells[cell_index];
+            sdl_poke_hole(renderer, &grid_cell->rect);
+
             // Todo: connect with surrounding cells using Disjoint-set
             // Todo: check if the Disjoint-set percolates
             // Todo: (if simple enough) highlight percolating area in blue
         }
-
-        for (int i = 0; i < grid_cell_count; i++) {
-            GridCell* grid_cell = &grid_cells[i];
-            if (!grid_cell->is_open) continue;
-
-            sdl_poke_hole(renderer, &grid_cell->rect);
-        }
-
-        // Todo: Optimise this so that it doesn't draw the grid every loop (just shrink the size of the grid_cursor by a pixel and render once) - is that even possible?
-        sdl_draw_grid_outlines(grid_dimensions, renderer);
 
         SDL_RenderPresent(renderer);
         mouse_clicked = false;
